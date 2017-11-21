e24ac597076c679725a80c5785a08af0db05dbf1
righisarah <righi.sarah1995@gmail.com>
2017-11-21 10:36:54 +0100

    recuperation version manon

--- projet_int/index.php
+++ projet_int/index.php
@@ -1,72 +1,29 @@
-<!--
-Author: Boutaina.Didioui
--->
-<!DOCTYPE HTML> 
+<!DOCTYPE HTML>
 <html>
 	<head>
 		<?php include 'templates/includes/$head.html' ?>
 	</head>
 
-<body>
+	<body>
 
-		<?php include 'templates/includes/$navigation.html'?>
-		<?php include 'templates/includes/$banner.html' ?>
+		<!-- bar de menu -->
 
-	<div class="banner" id="home">
-		<div class="banner-content">
-			<h1>Chifaa</h1>
-			<h2>Association Humanitaire</h2>
+			<div class="navigation">
+				<?php include 'templates/includes/$navigation.html' ?>
+			</div>
+
+		<!-- banner -->
+
+		<div class="banner" id="home">
+			<?php include 'templates/includes/$banner.html' ?>
 		</div>
-	</div>
-<section id="principe">
-		<div class="container">
-				<div class="row">
-					
-					<div class="principe">
-					<h1>
-					Principe 
-					</h1>
-					<p> 
-					Notre principe:
-					lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum umlorem ipsum 
-					</p>	
-					</div>
-				</div>	
-		</div>
-</section>
+	
 
+		<!-- aller vers le haut -->
+		<a href="#" id="toTop" style="display: inline;">
+			<span id="toTopHover"></span>To Top
+		</a>
 
-<section id="Valeurs">
-		<div class="container">
-				<div class="row">
-					<div class="valeurs">
-								<h1> Valeurs: </h1>
-							</div>
-					</div>
-				</div>	
-		</div>
-</section>
-
-<section id="Nos actions">
-		<div class="container">
-				<div class="row">
-					<div class="Nosaction">
-							<h1> Nos actions : </h1>
-					</div>
-				</div>	
-		</div>
-</section>
-
-
-<section id="Qui sommes-nous?">
-		<div class="container">
-				<div class="row">
-					<div class="Quisommes">
-						<h1> Qui sommes-nous ? </h1>	
-					</div>
-				</div>	
-		</div>
-</section>
-<?php include 'templates/includes/$footpage.html' ?>
-</body>
+	</body>
+	
 </html>
