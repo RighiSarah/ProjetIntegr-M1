c765d769b88d18e4266370be7f2c1557a8f3e8b1
righisarah <righi.sarah1995@gmail.com>
2017-11-25 22:38:37 +0100

    FAQ
    page : lecture de réponses (messages) à une question

--- projet_int/final_projet_int/lire_sujet.php
+++ projet_int/final_projet_int/lire_sujet.php
@@ -1,20 +1,20 @@
+<?php
+session_start();
+ini_set('display_errors', 1); //afficher les warnings
+?>
+
 <html>
 	<head>
 			<title>Lecture d'un sujet</title>
 	 		<?php include 'templates/includes/$head.html' ?>
-	        <style>
-	            #map {height: 240px;
-	                    width: 130%;
-	                }
-	           
-	        </style>
+	      
 			<link href="templates/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
 	                <link href="templates/fonts" rel="stylesheet"  media="all" />
 			<script src="templates/js/bootstarp.min.js"></script>
 	</head>
 <body>
 		 <div class="navigation">
-            <?php include 'templates/includes/$navigation_intranet.html' ?>
+            <?php include 'templates/includes/$navigation.php' ?>
          </div>
 		  </br>
           </br>
@@ -26,78 +26,63 @@ if (!isset($_GET['id_sujet_a_lire'])) {
 }
 else {
 ?>	
-	                <div class="row col-md-10 col-md-offset-1 custyle">
-                <table class="table table-striped custab" id ="myTable">
-                <thead>
- 
-         
-                    <tr>
-                        <th>Sujet</th>
-                        <th>Auteur</th>
-                       <th>Message</th>
-                      
-                    </tr>
-                 </thead>
-
-
+	  <div class="row col-md-8 col-md-offset-2 custyle">
+	   	<div class="row">
+            <h1> Réponses au sujet </h1>
+        </div>
+     	 </br>
+         </br>
+      </div>
+      <div class="row">
+         <div class="col-md-2 "></div> 
+          	<div class="btn-group btn-group-lg col-md-offset-2">
+ 				 <a href="FAQ.php" type="button" class="btn btn-primary" ><span class="glyphicon glyphicon-arrow-left"></span>   Retour au Forum</a>
+ 				 <a  href="./insert_reponse.php?numero_du_sujet=<?php echo $_GET['id_sujet_a_lire']; ?>" type="button" class="btn btn-primary"> <span class="glyphicon glyphicon-comment"></span>   Répondre</a>
+ 				
+			</div>
+        </div>
 	<?php
 	// on se connecte à notre base de données
-	try
-		{
-			$base = new PDO('mysql:host=localhost;dbname=chifaa;charset=utf8', 'root', '');
-		}
-			catch (Exception $e)
-			{
-        		die('Erreur : ' . $e->getMessage());
-			}
-
-	// on prépare notre requête
-	//$sql = 'SELECT auteur, message, date_reponse FROM forum_reponses WHERE correspondance_sujet="'.$_GET['id_sujet_a_lire'].'" ORDER BY date_reponse ASC';
-	$sql = $base-> query ('SELECT auteur, message , date_reponse FROM forum_reponses WHERE correspondance_sujet="'.$_GET['id_sujet_a_lire'].'" ORDER BY date_reponse ASC');
-	// on lance la requête (mysql_query) et on impose un message d'erreur si la requête ne se passe pas bien (or die)
-	//$req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
-	//$sql ->execute(array ($_GET['id_sujet_a_lire']));
-	// on va scanner tous les tuples un par un
-	while ($data = $sql->fetch()) {
-
-	// on décompose la date
-	sscanf($data['date_reponse'], "%4s-%2s-%2s %2s:%2s:%2s", $annee, $mois, $jour, $heure, $minute, $seconde);
-
-	// on affiche les résultats
-	echo '<tr>';
-	echo '<td>';
-
-	// on affiche le nom de l'auteur de sujet ainsi que la date de la réponse
-	echo htmlentities(trim($data['auteur']));
-	//echo '<br />';
-	//echo $jour , '-' , $mois , '-' , $annee , ' ' , $heure , ':' , $minute;
-
-	echo '</td><td>';
-
-	// on affiche le message
-	echo nl2br(htmlentities(trim($data['message'])));
-	echo '</td></tr>';
-	}
-
-	// on libère l'espace mémoire alloué pour cette reqête
-	//mysql_free_result ($sql);
-	// on ferme la connection à la base de données.
-
-	$sql->closeCursor();
 	
-	?>
+              include 'connect_db.php' ;
+                try
+              {
+                 $base = new PDO('mysql:host='.$servername.';dbname='.$dbname.';charset=utf8', $username, $password);
+           	 }
+              catch (Exception $e)
+                {
+                 die('Erreur : ' . $e->getMessage());
+                }
 
-	<!-- on ferme notre table html -->
-	</table></div>
-	<br /><br />
-	<!-- on insère un lien qui nous permettra de rajouter des réponses à ce sujet -->
-	<a href="./insert_reponse.php?numero_du_sujet=<?php echo $_GET['id_sujet_a_lire']; ?>">Répondre</a>
+		// on prépare notre requête
+		$sql = $base-> query ('SELECT auteur, message , date_reponse FROM forum_reponses WHERE correspondance_sujet="'.$_GET['id_sujet_a_lire'].'" ORDER BY date_reponse ASC');
+
+	while ($data = $sql->fetch()) { ?>
+		          </br>
+		<div class="row col-md-8 col-md-offset-2 custyle ">
+			<div class =" panel panel-primary">
+				<?php // on décompose la date
+					sscanf($data['date_reponse'], "%4s-%2s-%2s %2s:%2s:%2s", $annee, $mois, $jour, $heure, $minute, $seconde);
+				 ?>
+	
+				 <div class="panel panel-primary panel-heading"><span class="glyphicon glyphicon-user"></span> <?php echo htmlentities(trim($data['auteur']))?>
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class="glyphicon glyphicon-calendar"></span>  <?php echo htmlentities(trim($data['date_reponse'])); ?>
+				 </div>
+ 	 			<div class="panel-body">
+ 	 				<?php echo nl2br(htmlentities(trim($data['message']))); ?> 
+ 	 			</div>
+			</div>
+		</div>
 	<?php
-}
-?>
+		}
+
+		$sql->closeCursor();
+	
+		?>
+	<br /><br />
+	<?php } ?>
 <br /><br />
-<!-- on insère un lien qui nous permettra de retourner à l'accueil du forum -->
-<a href="FAQ.php">Retour à l'accueil</a>
+</div>
 
 </body>
 </html>
